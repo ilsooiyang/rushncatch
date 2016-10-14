3232664e3aae0a5005991f050ce88dcaa4331ffa
ilsooiyang <ilsooiyang@gmail.com>
2016-10-14 11:03:33 +0900

    deploy to heroku 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
 	root "players#home"
-=======
->>>>>>> 5a6013f10886859aebdb0f5bdc0f162d530e9742
 	get "home" => "players#home"
   resources :players do
   	resources :blogs, :only => [:index, :show, :new]
