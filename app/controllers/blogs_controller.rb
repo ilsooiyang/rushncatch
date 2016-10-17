069f19c9fb52b7b345cc7ff23dc915c5b21fdc3a
ilsooiyang <ilsooiyang@gmail.com>
2016-10-17 15:52:55 +0900

    bootstrapped home.html.erb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,23 +5,23 @@ class BlogsController < ApplicationController
 		@blogs = Blog.all
 	end
 
-	def index #shows all blogs of a player
-		@player = Player.find(params[:player_id])
+	def index #shows all blog posts of a player
+		set_player
 		@blogs = @player.blogs
 	end
 
-	def show #shows all blogs of a player
-		@player = Player.find(params[:player_id])
-		@blogs = @player.blogs
+	def show #shows one blog post of a player
+		set_player
+		@blog = @player.blogs.find(params[:id])
 	end
 
 	def new
-		@player = Player.find(params[:player_id])
+		set_player
 		@blog = @player.blogs.new
 	end
 
 	def create
-		@player = Player.find(params[:player_id])
+		set_player
 		@blog = @player.blogs.new(blog_params)
 		if @blog.save
 			redirect_to news_path, notice: "Successfully Posted"
@@ -30,10 +30,21 @@ class BlogsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@blog = Blog.find(params[:id])
+		@player = Player.find(@blog.player_id)
+		@blog.destroy
+		redirect_to player_blogs_path(@player), notice: "Successfully Deleted"
+	end
+
 private
 
 	def blog_params
 		params.require(:blog).permit(:content)
+	end
+
+	def set_player
+		@player = Player.find(params[:player_id])
 	end
 
 end
